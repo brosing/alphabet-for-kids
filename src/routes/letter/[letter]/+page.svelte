7233d1aedb50dce44aaaaa335588dc2a7198160f
Singgih Nn <singgihnn@gmail.com>
2025-03-13 00:46:06 +0700

    fix data

--- src/routes/letter/[letter]/+page.svelte
+++ src/routes/letter/[letter]/+page.svelte
@@ -1,68 +1,15 @@
 <script lang="ts">
   import { ArrowLeftIcon, ArrowUpIcon, ArrowDownIcon, Volume2Icon } from 'svelte-feather-icons';
   import { page } from '$app/state';
+  import { enEmoji, idEmoji } from '$lib/data';
 
   type LangType = 'en' | 'id'
   const lang = page.url.searchParams.get('lang') as LangType;
   const letter = page.params.letter.toUpperCase();
   let isUpperCase = true;
   const words: Record<LangType, { [key: string]: string[]}> = {
-    en: {
-      A: ['🍎 Apple', '✈️ Airplane', '🐜 Ant'],
-      B: ['🍌 Banana', '🐻 Bear', '🎈 Balloon'],
-      C: ['🐱 Cat', '🍪 Cookie', '🤡 Clown'],
-      D: ['🐕 Dog', '🦌 Deer', '🎲 Dice'],
-      E: ['🥚 Egg', '🐘 Elephant', '👁️ Eye'],
-      F: ['🐟 Fish', '🦊 Fox', '🔥 Fire'],
-      G: ['🦒 Giraffe', '🍇 Grapes', '🎸 Guitar'],
-      H: ['🏠 House', '🐎 Horse', '🎩 Hat'],
-      I: ['🍦 Ice Cream', '🦎 Iguana', '🏝️ Island'],
-      J: ['🤹 Juggler', '🎸 Jazz', '🦘 Kangaroo'],
-      K: ['🪁 Kite', '🔑 Key', '👑 King'],
-      L: ['🦁 Lion', '🍃 Leaf', '💡 Light'],
-      M: ['🌙 Moon', '🐒 Monkey', '🎵 Music'],
-      N: ['👃 Nose', '📰 Newspaper', '🌙 Night'],
-      O: ['🐙 Octopus', '🍊 Orange', '🦉 Owl'],
-      P: ['🐼 Panda', '🖊️ Pen', '🍕 Pizza'],
-      Q: ['👸 Queen', '🪆 Quilt', '❓ Question'],
-      R: ['🌹 Rose', '🎀 Ribbon', '🚀 Rocket'],
-      S: ['🌞 Sun', '⭐ Star', '🐍 Snake'],
-      T: ['🐯 Tiger', '🌳 Tree', '🚂 Train'],
-      U: ['☂️ Umbrella', '🦄 Unicorn', '👆 Up'],
-      V: ['🎻 Violin', '🌋 Volcano', '🚐 Van'],
-      W: ['🐺 Wolf', '💧 Water', '🌊 Wave'],
-      X: ['📦 Box', '🎸 Xylophone', '❌ X-mark'],
-      Y: ['🟡 Yellow', '🧒 Youth', '☯️ Yin-yang'],
-      Z: ['🦓 Zebra', '🤐 Zipper', '⚡ Zigzag']
-    },
-    id: {
-      A: ['🍎 Apel', '🐜 Angsa', '🚑 Ambulans'],
-      B: ['🍌 Bebek', '🐻 Beruang', '🎈 Balon'],
-      C: ['🐱 Cicak', '🍪 Coklat', '🥥 Cabai'],
-      D: ['🦌 Domba', '🎲 Dadu', '🍇 Durian'],
-      E: ['🐘 Elang', '🥚 Entok', '🦐 Udang'],
-      F: ['🦊 Flamingo', '🐟 Ikan', '🍟 Kentang'],
-      G: ['🦒 Gajah', '🦍 Gorila', '🎸 Gitar'],
-      H: ['🦛 Harimau', '🐎 Hamster', '🦅 Helang'],
-      I: ['🐟 Ikan', '🦎 Iguana', '🍦 Es'],
-      J: ['🦒 Jerapah', '🕷️ Jangkrik', '🍊 Jeruk'],
-      K: ['🐊 Katak', '🦘 Kanguru', '🐈 Kucing'],
-      L: ['🦁 Lebah', '🦎 Laba-laba', '🦗 Lalat'],
-      M: ['🐒 Monyet', '🐜 Macan', '🐁 Marmut'],
-      N: ['🐄 Naga', '🦅 Nuri', '🐝 Nyamuk'],
-      O: ['🦉 Orangutan', '🐑 Orang Utan', '🦦 Otter'],
-      P: ['🦜 Pinguin', '🐼 Panda', '🦚 Puyuh'],
-      Q: ['🐜 Qilin'],
-      R: ['🦏 Rusa', '🐟 Rajungan', '🦊 Rubah'],
-      S: ['🐍 Singa', '🦈 Sapi', '🐿️ Semut'],
-      T: ['🐯 Tikus', '🦃 Tupai', '🐅 Trenggiling'],
-      U: ['🦅 Ulat', '🐍 Ular', '🦉 Unggas'],
-      V: ['🦊 Vampir'],
-      W: ['🐊 Walet', '🦅 Wau-wau'],
-      X: ['❌ Tidak ada kata dalam bahasa Indonesia yang dimulai dengan X'],
-      Y: ['🦓 Yak'],
-      Z: ['🦓 Zebra']
-    }
+    en: enEmoji,
+    id: idEmoji
   };
   const selectLang = lang === 'en' ? 'en-US' : 'id-ID';
 
